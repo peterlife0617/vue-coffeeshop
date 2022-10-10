f7854456895e19c8a8617a7ca9157788de92ceea
PeterChuang <peterlife0617@gmail.com>
2022-10-10 14:30:51 +0800

    建立專案

---
+++ src/components/OrderList.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref, reactive } from "vue";
+
+const detail = ref({
+  name: "",
+  price: 0,
+  size: "",
+  notes: "",
+});
+console.log(detail);
+const list = reactive([
+  {
+    name: "拿鐵",
+    price: 70,
+    size: "l",
+    notes: "去冰",
+  },
+  {
+    name: "拿鐵",
+    price: 70,
+    size: "l",
+    notes: "去冰",
+  },
+]);
+</script>
+
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>name</th>
+          <th>price</th>
+          <th>size</th>
+          <th>notes</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in list" :key="item.id">
+          <td v-text="index + 1"></td>
+          <td v-text="item.name"></td>
+          <td v-text="item.price"></td>
+          <td v-text="item.size"></td>
+          <td v-text="item.price"></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+ import OrderList from '@/components/OrderList.vue'
+</script>
+
+<template>
+  <OrderList></OrderList>
+</template>
